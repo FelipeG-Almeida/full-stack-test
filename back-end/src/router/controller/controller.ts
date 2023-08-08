a738b09239c4382414cda6f32380b68ab7c38ef3
Felipe Almeida <felipegustavotheboss@gmail.com>
2023-08-08 11:43:32 -0300

    back/front dir organization

---
+++ back-end/src/router/controller/controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+
+export class Controller {
+    public uploadCSV = async (req: Request, res: Response) => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
