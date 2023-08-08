1c4b1929cb0bda81b05855d80c31df412f34fd0e
Felipe <felipegustavotheboss@gmail.com>
2023-08-08 07:21:25 -0300

    Initial commit

---
+++ src/router/router.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import { Controller } from './controller/controller';
+
+export const router = express.Router();
+
+const controller = new Controller();
+
+router.post('/api/files', controller);
